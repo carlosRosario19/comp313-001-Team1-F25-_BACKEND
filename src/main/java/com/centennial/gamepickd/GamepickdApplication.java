a919685d2340c66e48b059e4a66faa5bf2c0b2b1
Carlos Rosario <crosari5@my.centennialcollege.ca>
2025-09-23 20:38:22 -0400

    Creating Project

---
+++ src/main/java/com/centennial/gamepickd/GamepickdApplication.java
@@ -0,0 +1,13 @@
+package com.centennial.gamepickd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepickdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamepickdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centennial/gamepickd/GamepickdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centennial.gamepickd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamepickdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
